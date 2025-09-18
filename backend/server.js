19d0637ed733f86249985108cfd3e599336138e2
Siddhant_Mul <siddhantmul18@gmail.com>
2025-09-18 23:28:08 +0530

    implemented socket.io

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,11 @@
 const app = require("./src/app");
 require("dotenv").config();
 const connectDb = require("./src/db/db");
+const socketServer = require("./src/sockets/socket.io");
+const http = require("http").createServer(app);
 
+socketServer(http);
 connectDb();
-app.listen(process.env.PORT, () => {
+http.listen(process.env.PORT, () => {
   console.log("server start");
 });
