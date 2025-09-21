88565951a402ded053fdfb9dee2b910cb2db51b2
Siddhant_Mul <siddhantmul18@gmail.com>
2025-09-21 06:02:07 +0530

    fixed all error related socket.io

--- backend/src/services/socket.service.js
+++ backend/src/services/socket.service.js
@@ -37,37 +37,35 @@ function socketServer(http) {
   });
   io.on("connection", async (socket) => {
     socket.on("userPrompt", async (data) => {
-      const { chatId, prompt } = JSON.parse(data);
-      console.log(chatId, prompt);
-
       const userId = socket.user._id;
+      const parsedData = typeof data === "string" ? JSON.parse(data) : data;
+      const { chatId, prompt } = parsedData;
       try {
-        let chat;
-        if (chatId) {
-          chat = await chatModel.findById(chatId);
-        } else {
-          chat = await chatModel.create({
-            user: userId,
-            messageHistory: [],
-          });
-        }
-        console.log(chat);
+        let chat = chatId
+          ? await chatModel.findById(chatId)
+          : await chatModel.create({ user: userId, messageHistory: [] });
 
-        // chat.messageHistory.push({ sender: "user", message: prompt });
-        // const messageHistoryForGemini = chat.messageHistory.map((content) => {
-        //   const role = content.sender === "user" ? "user" : "model";
-        //   return {
-        //     role: role,
-        //     parts: [{ text: content.message }],
-        //   };
-        // });
-        // const AiResponse = await generateAiResponse(messageHistoryForGemini);
-        // chat.messageHistory.push({ sender: "model", message: AiResponse });
-        // socket.emit("aiResponse", { response: AiResponse, chatID: chat._id });
+        chat.messageHistory.push({ sender: "user", message: prompt });
 
-        // await chat.save();
+        const messageHistoryForGemini = chat.messageHistory.map((content) => {
+          const role = content.sender === "user" ? "user" : "model";
+          return {
+            role: role,
+            parts: [{ text: content.message }],
+          };
+        });
+        const AiResponse = await generateAiResponse(messageHistoryForGemini);
+        chat.messageHistory.push({ sender: "model", message: AiResponse });
+        socket.emit("aiResponse", {
+          response: AiResponse,
+          chatID: chat._id,
+        });
+        await chat.save();
       } catch (error) {
-        socket.emit("error", "An internal server error occurred.");
+        socket.emit(
+          "error",
+          "An internal server error occurred. Please try again."
+        );
       }
     });
     socket.on("disconnect", () => {
